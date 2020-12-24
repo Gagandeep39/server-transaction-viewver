119e50956c8be9848224f43f2c5bef171dbbccfc
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-24 19:25:42 +0530

    Updated balance controller

--- src/main/java/com/gagan/server/controller/BalanceController.java
+++ src/main/java/com/gagan/server/controller/BalanceController.java
@@ -6,25 +6,18 @@ import org.springframework.web.bind.annotation.*;
 import lombok.AllArgsConstructor;
 
 import com.gagan.server.model.BalanceDTO;
-import com.gagan.server.service.BalanceService;
-import javax.validation.Valid;
-
+import com.gagan.server.service.IBalanceService;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/api/balances", produces = MediaType.APPLICATION_JSON_VALUE)
 public class BalanceController {
 
-    private final BalanceService balanceService;
+    private final IBalanceService balanceService;
 
-    @GetMapping("/{id}")
-    public BalanceDTO getBalance(@PathVariable final Integer id) {
-        return balanceService.get(id);
-    }
-
-    @PutMapping("/{id}")
-    public void updateBalance(@PathVariable final Integer id, @RequestBody @Valid final BalanceDTO balanceDTO) {
-        balanceService.update(id, balanceDTO);
+    @GetMapping
+    public BalanceDTO getBalance() {
+        return balanceService.fetchBalanceById();
     }
 
 }
