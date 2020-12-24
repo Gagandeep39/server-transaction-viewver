b80b516a323b29c1c6f9c17f331383fd802a8471
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-24 12:32:53 +0000

    Added user details logic

--- src/main/java/com/gagan/server/service/JwtUserDetailsService.java
+++ src/main/java/com/gagan/server/service/JwtUserDetailsService.java
@@ -1,5 +1,28 @@
 package com.gagan.server.service;
 
-public class JwtUserDetailsService {
+import java.util.ArrayList;
+
+import com.gagan.server.domain.User;
+import com.gagan.server.repos.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService  implements UserDetailsService {
+
+  @Autowired
+  private UserRepository userRepository;
+  
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository
+    .findById(Integer.parseInt(username))
+    .orElseThrow(() -> new UsernameNotFoundException("User " + username + " doesn't exist"));
+    return new org.springframework.security.core.userdetails.User(user.getUserid().toString(), user.getPassword(), new ArrayList<>());
+	}
   
 }
