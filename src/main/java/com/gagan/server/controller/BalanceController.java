f2c01491b8d265feecebae32a1fdf20d8cc48520
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-24 18:55:26 +0530

    Removed uncessary methods

--- src/main/java/com/gagan/server/controller/BalanceController.java
+++ src/main/java/com/gagan/server/controller/BalanceController.java
@@ -3,48 +3,28 @@ package com.gagan.server.controller;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.AllArgsConstructor;
+
 import com.gagan.server.model.BalanceDTO;
 import com.gagan.server.service.BalanceService;
-import java.util.List;
 import javax.validation.Valid;
-import org.springframework.http.HttpStatus;
 
 
 @RestController
+@AllArgsConstructor
 @RequestMapping(value = "/api/balances", produces = MediaType.APPLICATION_JSON_VALUE)
 public class BalanceController {
 
     private final BalanceService balanceService;
-
-    public BalanceController(final BalanceService balanceService) {
-        this.balanceService = balanceService;
-    }
-
-    @GetMapping
-    public List<BalanceDTO> getAllBalances() {
-        return balanceService.findAll();
-    }
 
     @GetMapping("/{id}")
     public BalanceDTO getBalance(@PathVariable final Integer id) {
         return balanceService.get(id);
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Integer createBalance(@RequestBody @Valid final BalanceDTO balanceDTO) {
-        return balanceService.create(balanceDTO);
-    }
-
     @PutMapping("/{id}")
     public void updateBalance(@PathVariable final Integer id, @RequestBody @Valid final BalanceDTO balanceDTO) {
         balanceService.update(id, balanceDTO);
-    }
-
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteBalance(@PathVariable final Integer id) {
-        balanceService.delete(id);
     }
 
 }
--- src/main/java/com/gagan/server/controller/TransactionsController.java
+++ src/main/java/com/gagan/server/controller/TransactionsController.java
@@ -3,27 +3,19 @@ package com.gagan.server.controller;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.AllArgsConstructor;
+
 import com.gagan.server.model.TransactionsDTO;
 import com.gagan.server.service.TransactionsService;
-import java.util.List;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 
-
 @RestController
+@AllArgsConstructor
 @RequestMapping(value = "/api/transactions", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TransactionsController {
 
     private final TransactionsService transactionsService;
-
-    public TransactionsController(final TransactionsService transactionsService) {
-        this.transactionsService = transactionsService;
-    }
-
-    @GetMapping
-    public List<TransactionsDTO> getAllTransactionss() {
-        return transactionsService.findAll();
-    }
 
     @GetMapping("/{id}")
     public TransactionsDTO getTransactions(@PathVariable final Integer id) {
@@ -34,17 +26,6 @@ public class TransactionsController {
     @ResponseStatus(HttpStatus.CREATED)
     public Integer createTransactions(@RequestBody @Valid final TransactionsDTO transactionsDTO) {
         return transactionsService.create(transactionsDTO);
-    }
-
-    @PutMapping("/{id}")
-    public void updateTransactions(@PathVariable final Integer id, @RequestBody @Valid final TransactionsDTO transactionsDTO) {
-        transactionsService.update(id, transactionsDTO);
-    }
-
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteTransactions(@PathVariable final Integer id) {
-        transactionsService.delete(id);
     }
 
 }
