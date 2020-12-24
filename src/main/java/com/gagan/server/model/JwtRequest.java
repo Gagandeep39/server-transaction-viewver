85ff563c61b6ebfc1f87d13c24727a4f0fa47a5c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-24 18:52:59 +0530

    Added request, response dto

--- src/main/java/com/gagan/server/model/JwtRequest.java
+++ src/main/java/com/gagan/server/model/JwtRequest.java
@@ -1,5 +1,17 @@
 package com.gagan.server.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JwtRequest {
+
+  private static final long serialVersionUID = 5926468583005150707L;
+	
+	private Integer userId;
+	private String password;
   
 }
