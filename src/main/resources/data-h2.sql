8f3772c47ef8266c48769f0bd1002dc9382bd693
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-24 18:52:11 +0530

    Added resources

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,6 @@
+INSERT INTO user (userid, password) VALUES (100001,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K');
+INSERT INTO user (userid, password) VALUES (100002,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K');
+INSERT INTO user (userid, password) VALUES (100003,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K');
+INSERT INTO user (userid, password) VALUES (100004,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K');
+INSERT INTO user (userid, password) VALUES (100005,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K');
+INSERT INTO user (userid, password) VALUES (100006,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K');
